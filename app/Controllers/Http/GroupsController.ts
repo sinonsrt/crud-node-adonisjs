271d0f6a861062d7605242c96bc6596e19345da6
Lucas Marchiori <sinonsrt@hotmail.com>
2021-03-19 13:09:55 -0300

    Add Group

---
+++ app/Controllers/Http/GroupsController.ts
@@ -0,0 +1,8 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Group from 'App/Models/Group'
+
+export default class GroupsController {
+    public async index({ request }){
+        
+    }
+}
